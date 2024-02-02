a22ad0b89119d90c320c758094006db8730c974d
WAHJADAI <drafzaza123@icloud.com>
2024-02-02 20:43:25 +0700

    change color Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 const Nav = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-sky-600">
       <a className="btn btn-ghost text-xl">daisyUI</a>
     </div>
   )
