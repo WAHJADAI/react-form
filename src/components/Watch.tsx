46ec04cc840afbecf1186a1a3ac22b5c25d10bb6
WAHJADAI <drafzaza123@icloud.com>
2024-02-06 19:42:50 +0700

    style css

--- src/components/Watch.tsx
+++ src/components/Watch.tsx
@@ -6,15 +6,19 @@ const Watch = ({ control }: { control: Control<InputsForm> }) => {
 
   return (
     <>
-      <p>{watchAll.date}</p>
-      <p>{watchAll.time}</p>
-      <p>{watchAll.channel}</p>
-      <p>{watchAll.name}</p>
-      <p>{watchAll.phone}</p>
-      <p>{watchAll.classified}</p>
-      <p>{watchAll.priority}</p>
-      <p>{watchAll.relatedParties}</p>
-      <p>{watchAll.detail}</p>
+      <div className="flex justify-center mt-3">
+        <div className="card  bg-slate-200 border-2 border-slate-300 w-4/6 p-2">
+          <p>{watchAll.classified}</p>
+          <p>{watchAll.date}</p>
+          <p>{watchAll.time}</p>
+          <p>{watchAll.channel}</p>
+          <p>{watchAll.name}</p>
+          <p>{watchAll.phone}</p>
+          <p>{watchAll.priority}</p>
+          <p>{watchAll.relatedParties}</p>
+          <p>{watchAll.detail}</p>
+        </div>
+      </div>
     </>
   )
 }
